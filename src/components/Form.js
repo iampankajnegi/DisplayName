188e3b581b47b09c0e203f340c3df906c4522202
unknown <negi60239@gmail.com>
2024-04-24 17:21:39 +0530

    task5

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,71 +1,48 @@
-import React from 'react'
-import { useState } from 'react'
-import "./form.css"
+import React, { useState } from 'react';
+import './form.css';
 
-const Myform = () => {
- 
-    const [formData , setFormData] = useState({
+const MyForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+  });
 
-            firstName : "" ,
+  const [fullName, setFullName] = useState('');
 
-            lastName : "" ,
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const newFullName = `${formData.firstName} ${formData.lastName}`;
+    setFullName(newFullName);
+  };
 
-
-    })
-
-    const [fullName, setFullName] = useState('');
-    
-  
-
-
-    const handleSubmit = (e) =>{
-
-        e.preventDefault()
-
-        let newFullName = `${formData.firstName} ${formData.lastName}`;
-        setFullName(newFullName)
-        
+  const handleChange = (e) => {
+    // Use a regular expression to allow only letters and spaces for the first and last names
+    const regex = /^[a-zA-Z\s]*$/;
+    const { name, value } = e.target;
+    if (regex.test(value) || value === '') {
+      // Allow empty value or valid input
+      setFormData({ ...formData, [name]: value });
     }
-
-     const handleChange = (e)=>{
-
-          setFormData({...formData,[e.target.name]:e.target.value})
-
-     }
-     
-
+  };
 
   return (
     <div>
-        <div className="form-display">
-                 
-                 <h1>Full Name Display</h1>
-
-            <form  onSubmit={handleSubmit}> 
-                
-                <label htmlFor="firstName"> 
-                First Name: </label>
-                <input type="text" name="firstName"  id="firstName"  value={formData.firstName} onChange={handleChange} required/>
-                
-                <br/>
-
-                
-                <label htmlFor="lastName">
-                    Last Name: </label>
-                <input type="text" name="lastName"   id="lastName" value={formData.lastName} onChange={handleChange} required/>
-               
-            <br/>
-                
-                <button type="submit"> Submit </button>
-             
-
-            </form>
-
-          full name : {fullName}
-        </div>
+      <div className="form-display">
+        <h1>Full Name Display</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" name="firstName" id="firstName" value={formData.firstName} onChange={handleChange} required />
+          <br />
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" name="lastName" id="lastName" value={formData.lastName} onChange={handleChange} required />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+        Full Name: {fullName}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Myform 
+export default MyForm;
